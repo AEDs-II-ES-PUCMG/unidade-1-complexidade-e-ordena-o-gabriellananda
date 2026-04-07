5736e2b99ee903dea03870d02a15fabcec22a33b
gabriellananda <gabriella-nanda@hotmail.com>
2026-04-06 22:24:28 -0300

    Implementa classe Mergesort

---
+++ src/MergeSort.java
@@ -0,0 +1,95 @@
+public class MergeSort<T extends Comparable<T>> implements IOrdenador<T> {
+    private int comparacoes;
+    private int movimentacoes;
+    private double tempoOrdenacao;
+    private double inicio;
+
+    private double nanoToMilli = 1.0/1_000_000;
+
+    @Override
+    public int getComparacoes() {
+        return comparacoes;
+    }
+
+    @Override
+    public int getMovimentacoes() {
+        return movimentacoes;
+    }
+
+    @Override
+    public double getTempoOrdenacao() {
+        return tempoOrdenacao;
+    }
+
+    private void iniciar(){
+        this.comparacoes = 0;
+        this.movimentacoes = 0;
+        this.inicio = System.nanoTime();
+    }
+    
+    /**
+    * Algoritmo de ordenação Mergesort.
+    * @param int esq: início do array a ser ordenado
+    * @param int dir: fim do array a ser ordenado
+    */
+    // 1.a chamada do método mergesort: esq: 0; dir: array.length - 1
+    private void mergesort(int[] array, int esq, int dir) {
+        if (esq < dir) {
+            int meio = (esq + dir) / 2;
+            mergesort(array, esq, meio);
+            mergesort(array, meio + 1, dir);
+            intercalar(array, esq, meio, dir);
+        }
+    }
+
+    /**
+    * Algoritmo que intercala os elementos localizados entre as posições esq e dir
+    * @param int esq: início do array a ser ordenado
+    * @param int meio: posição do meio do array a ser ordenado
+    * @param int dir: fim do array a ser ordenado
+    */ 
+    private void intercalar(int[] array, int esq, int meio, int dir) {
+
+        int n1, n2, i, j, k;
+
+            //Definir tamanho dos dois subarrays
+            n1 = meio - esq + 1;
+            n2 = dir - meio;
+
+            int[] a1 = new int[n1];
+            int[] a2 = new int[n2];
+
+            //Inicializar primeiro subarray
+            for (i = 0; i < n1; i++) {
+                a1[i] = array[esq + i];
+            }
+
+            //Inicializar segundo subarray
+            for (j = 0; j < n2; j++) {
+                a2[j] = array[meio + j + 1];
+            }
+
+        //Intercalação propriamente dita
+            for (i = j = 0, k = esq; (i < n1 && j < n2); k++) {
+                if (a1[i] <= a2[j])
+                array[k] = a1[i++];
+            else
+                array[k] = a2[j++];
+            }
+        
+        if (i == n1)
+            for (; k <= dir; k++) {
+                    array[k] = a2[j++];
+                }
+        else
+                for (; k <= dir; k++) {
+                    array[k] = a1[i++];
+                }
+    }
+
+    @Override
+	public T[] ordenar(T[] dados) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'ordenar'");
+	}
+}
